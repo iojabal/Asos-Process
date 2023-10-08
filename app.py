ad65f970b35a2f1fe112243c4f7ce79ac0df9496
iojabal <iojabal9@gmail.com>
2023-10-08 17:33:17 -0400

    FCFSAdded

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 from models.Process import Process
-from models.Algorithms import Algorithms, Priority
+from models.Algorithms import Algorithms, Priority, SJF, FCFS
 
 app = Flask(__name__)
 
 algorithm = Algorithms()
+
+
 @app.route("/", methods=["POST"])
 def loadProcesses():
     algorithm.processes = []
@@ -25,8 +27,39 @@ def priorityFunction():
     priority.readyQueue = priority.sortQueue(priority.processes)
     priority.doAlgorithm()
     priority.showExecutionQueue()
-    return {"Message": "ok"}
+    ququ = []
+    for process in priority.queue:
+        a = [str(p) for p in process]
+        ququ.append(a)
+    return {"Message": ququ}
 
+@app.route("/SJF", methods=["POST"])
+def sjfFunction():
+    sjf = SJF()
+    sjf.processes = algorithm.processes
+    sjf.readyQueue = sjf.sortQueue(sjf.processes)
+    sjf.doAlgorithm()
+    sjf.showExecutionQueue()
+    sjf.showReadyQueue()
+    ququ = []
+    for process in sjf.queue:
+        a = [str(p) for p in process]
+        ququ.append(a)
+    return {"Message": ququ}
+
+
+@app.route("/FCFS", methods=['POST'])
+def fcfsFunction():
+    fcfs = FCFS()
+    fcfs.processes = algorithm.processes
+    fcfs.doAlgorithm()
+
+    ququ = []
+    for process in fcfs.queue:
+        a = [str(p) for p in process]
+        ququ.append(a)
+    fcfs.showExecutionQueue()
+    return {"message": ququ}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models/Algorithms.py
+++ models/Algorithms.py
@@ -1,16 +1,20 @@
+import copy
 import queue
 from copy import deepcopy
+
 
 class Algorithms:
     def __init__(self):
         self.processes = []
-        self.executionQueue = []
+        self.executionQueue = [0]
         self.readyQueue = []
+        self.queue = []
         self.ctx = None
         self.pr = False
         self.fcfs = False
         self.sjf = False
         self.srt = False
+
     def showReadyQueue(self):
         for process in self.readyQueue:
             print(process)
@@ -18,9 +22,29 @@ class Algorithms:
     def showProcesses(self):
         for process in self.processes:
             print(process)
+
     def showExecutionQueue(self):
         for process in self.executionQueue:
             print(process)
+
+    def showReadyQueue(self):
+        print("------------------------------------------------")
+        for process in self.queue:
+            a = [str(p) for p in process]
+            print(a)
+
+    def getHighestArrivedTime(self):
+        total_sum = 0
+        for process in self.processes:
+            total_sum += process.CPUTIME
+
+        return total_sum
+
+    def getProcessByIndex(self, index):
+        for process in self.processes:
+            if process.arrivedTime == index:
+                return process
+
 
 class Priority(Algorithms):
     def __init__(self):
@@ -30,15 +54,102 @@ class Priority(Algorithms):
         return sorted(q, key=lambda x: x.priority)
 
     def doAlgorithm(self):
-        for ready_process in self.readyQueue:
+        while self.readyQueue:
+            self.queue.append(copy.deepcopy(self.readyQueue))
+            ready_process = self.readyQueue.pop(0)
             self.executionQueue.append(ready_process)
-            for i in range(1, ready_process.CPUTIME):
+
+            for _ in range(1, ready_process.CPUTIME):
+                self.queue.append(copy.deepcopy(self.readyQueue))
                 p = deepcopy(ready_process)
                 p.CPUTIME -= 1
 
                 if ready_process.CPUTIME != 0:
                     self.executionQueue.append(p)
                 ready_process = p
+
             if self.ctx is not None:
-                for i in range(self.ctx):
+                for _ in range(self.ctx):
                     self.executionQueue.append(0)
+
+class SJF(Algorithms):
+    def __init__(self):
+        super().__init__()
+
+    def sortQueue(self, d):
+        return sorted(d, key=lambda x: x.CPUTIME)
+
+    # def doAlgorithm(self):
+    #     for ready_process in self.readyQueue:
+    #         self.executionQueue.append(ready_process)
+    #         for _ in range(1, ready_process.CPUTIME):
+    #             p = deepcopy(ready_process)
+    #             p.CPUTIME -= 1
+
+    #             if ready_process.CPUTIME != 0:
+    #                 self.executionQueue.append(p)
+    #             ready_process = p
+    #         if self.ctx is not None:
+    #             for _ in range(self.ctx):
+    #                 self.executionQueue.append(0)
+
+    def doAlgorithm(self):
+        while self.readyQueue:
+            self.queue.append(copy.deepcopy(self.readyQueue))
+            ready_process = self.readyQueue.pop(0)
+            self.executionQueue.append(ready_process)
+
+            for _ in range(1, ready_process.CPUTIME):
+                self.queue.append(copy.deepcopy(self.readyQueue))
+                p = deepcopy(ready_process)
+                p.CPUTIME -= 1
+
+                if ready_process.CPUTIME != 0:
+                    self.executionQueue.append(p)
+                ready_process = p
+
+            if self.ctx is not None:
+                for _ in range(self.ctx):
+                    self.executionQueue.append(0)
+
+class FCFS(Algorithms):
+    def __init__(self):
+        super().__init__()
+
+    def doAlgorithm(self):
+        i = 0
+        while i < self.getHighestArrivedTime():
+            pr = self.getProcessByIndex(i)
+            if pr is not None:
+                self.readyQueue.append(pr)
+            self.queue.append(copy.deepcopy(self.readyQueue))
+            ready_process = self.readyQueue.pop(0)
+
+            self.executionQueue.append(ready_process)
+            if ready_process is not None:
+                for _ in range(1, ready_process.CPUTIME):
+
+                    self.queue.append(copy.deepcopy(self.readyQueue))
+                    p = deepcopy(ready_process)
+                    p.CPUTIME -= 1
+
+                    if ready_process.CPUTIME != 0:
+                        self.executionQueue.append(p)
+                    ready_process = p
+                    i += 1
+                    proc = self.getProcessByIndex(i)
+                    if proc is not None:
+                        self.readyQueue.append(proc)
+
+                if self.ctx is not None:
+                    for _ in range(self.ctx):
+                        self.executionQueue.append(0)
+            i += 1
+
+
+        self.queue = ["0" if sublist == [] or sublist[0] == None else sublist for sublist in self.queue]
+
+class SRT(Algorithms):
+    def __init__(self):
+        super().__init__()
+
