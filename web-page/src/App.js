a16067ebb85fb5a25e1e44d295ef87b16e3d7576
iojabal <iojabal9@gmail.com>
2023-10-10 21:52:05 -0400

    Footer

--- web-page/src/App.js
+++ web-page/src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
-import UpperBar from './components/UpperBar/UpperBar.jsx'
+import "./App.css";
+import UpperBar from "./components/UpperBar/UpperBar.jsx";
+import { Footer } from "./components/Footer/Footer";
 function App() {
   return (
+    <>
       <UpperBar></UpperBar>
+      <div className="h-screen flex flex-col">
+        <main className="flex-grow"></main>
+
+        <Footer></Footer>
+      </div>
+    </>
   );
 }
 
