a77d7e08f4398e274741b22be66932bb7618f3da
andres21gutierrez <115100995+andres21gutierrez@users.noreply.github.com>
2023-10-10 23:24:52 -0400

    HOME-ACTUALIZADO

--- web-page/src/View/Home/Home.jsx
+++ web-page/src/View/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Table from '../../components/InputTable/InputTable.jsx'
+import { useState } from 'react';
 
 // PRIORIDAD MENOS TIEMPO DE LLEGADA
 // FCFS MENOS PRIORIDAD
@@ -8,14 +9,21 @@ import Table from '../../components/InputTable/InputTable.jsx'
 
 export default function Home(){
     var proceso = ''
+    const [cambioContexto, setCambioContexto] = useState(false);
+    // const [proceso, setProceso] = useState('');
     const onChangeValue = (event) => {
         proceso = event.target.value
         console.log(event.target.value);
     };
 
+    const handleCambioContextoChange = (event) => {
+        console.log(event.target.checked)
+        setCambioContexto(event.target.checked);
+      };
+
     return(
         <>
-            <div className='flex flex-col'>
+            <div className='flex flex-col sm:flex-row justify-center'>
                 <div className='flex flex-col p-5'>
                     <h2>SELECCIONE MÉTODO A RESOLVER:</h2>
                     <div className='flex flex-col justify-between max-w-lg p-4' onChange={onChangeValue}>
@@ -27,12 +35,32 @@ export default function Home(){
                     </div>
                 </div>
 
-                <div className=' flex flex-row p-5'>
-                    <label> INGRESE EL NUMERO DE PROCESOS:</label>
+                <div className=' flex md:flex-col justify-center px-5 space-x-3 items-center'>
+                    <label className='md:p-4'> INGRESE EL NUMERO DE PROCESOS:</label>
                     <input
                         type='number'
                         className=' border border-black text-center'
                     />
+                </div>
+
+                <div className='flex flex-col justify-center'>
+                    <div className=' flex flex-row p-5 space-x-3 items-center'>
+                        <label>¿CAMBIO DE CONTEXTO? </label>
+                        <input 
+                            type='checkbox'
+                            checked = {cambioContexto}
+                            onChange={handleCambioContextoChange}
+                            className='border border-black' 
+                            name='CContexto'></input>
+                    </div>
+                    {cambioContexto && (
+                        <div className='px-4'>
+                            <input
+                            className='border border-black text-center'
+                            type='number'
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
         
